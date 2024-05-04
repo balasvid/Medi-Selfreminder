0c012b3b324c62f4dd4d9d99194396e7ea887bba
balasvid <162349624+balasvid@users.noreply.github.com>
2024-05-04 22:56:27 +0200

    Add files via upload

---
+++ Roadmap Produkt_Mediliste.py
@@ -0,0 +1,23 @@
+text = """
+Roadmap Produkt 
+Version 1.0
+
+Funktionen:
+- Medikamentenhinzufügung: Benutzer können Medikamente hinzufügen, indem sie den Namen, die Dosierung und die Einnahmezeit eingeben.
+- Täglicher Überblick: Ein Dashboard zeigt eine Liste der für den Tag geplanten Medikamente an.
+- Einfaches Profil: Benutzer können ein Profil erstellen und grundlegende Informationen wie Name und Geburtsdatum speichern.
+
+Zeitplan:
+- Woche 1-2: Implementierung der Funktionen zur Medikamentenhinzufügung und Erstellung des Profils.
+- Woche 3-4: Entwicklung des täglichen Überblicks.
+- Woche 5: Durchführung von Tests und Fehlerbehebung.
+- Woche 6: Veröffentlichung der Version 1.0 als MVP.
+
+Speichern und Laden von Daten:
+Die Medikamentenliste-App wird lokal in Streamlit gespeichert. Beim Start der App werden vorhandene Daten geladen, falls vorhanden.
+
+Benutzerfreundlichkeit:
+Die Benutzeroberfläche ist einfach gestaltet, mit klaren Anweisungen und Schaltflächen für das Hinzufügen, Bearbeiten und Löschen von Medikamenten.
+"""
+
+
